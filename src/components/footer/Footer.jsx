2ef4d24ac52e48b1597e258e9e2dbb26e001db0c
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-05-05 12:56:33 +0500

    Todo-learn

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className="py-3 h2  m-0 text-center alert alert-info">
+        Made by Azizbek Programmer
+      </div>
+    );
+  }
+}
+
+export default Footer;
